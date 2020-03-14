1db9d6cb4ebfeb99544c7510e401d57a8678a542
taowhack1 <tulagarn007@gmail.com>
2020-03-14 23:17:54 +0700

    add class component to Header.js

--- sirivatana/src/Component/Header.js
+++ sirivatana/src/Component/Header.js
@@ -1,1 +1,12 @@
 import React from 'react'
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
